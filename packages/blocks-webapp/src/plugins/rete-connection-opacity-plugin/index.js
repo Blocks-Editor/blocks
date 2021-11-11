5053e2d37a26fec553676c54ba6b9406f42a4519
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-11-10 19:23:15 -0700

    Deactivate rete-connection-opacity-plugin due to edge cases on some devices / browsers

--- packages/blocks-webapp/src/plugins/rete-connection-opacity-plugin/index.js
+++ packages/blocks-webapp/src/plugins/rete-connection-opacity-plugin/index.js
@@ -1,17 +1,19 @@
 function install(editor, config = {}) {
 
-    let {minDistance = 500, opacityFalloff = 200} = config;
+    // const {minDistance = 500, opacityFalloff = 200} = config;
+
+    // TODO: fix edge cases causing zero opacity on some devices / browsers
 
     // Fade out distant connections
-    editor.on(['renderconnection', 'updateconnection', 'zoom'], () => {
-        setTimeout(() => {
-            for(let pathElement of editor.view.container.querySelectorAll('.connection .main-path')) {
-                let bounds = pathElement.getBoundingClientRect();
-                let distance = bounds.width;
-                pathElement.style.opacity = 1 / (1 + Math.sqrt(Math.max(distance - minDistance, 0) / opacityFalloff));
-            }
-        });
-    });
+    // editor.on(['renderconnection', 'updateconnection', 'zoom'], () => {
+    //     setTimeout(() => {
+    //         for(let pathElement of editor.view.container.querySelectorAll('.connection .main-path')) {
+    //             let bounds = pathElement.getBoundingClientRect();
+    //             let distance = bounds.width;
+    //             pathElement.style.opacity = 1 / (1 + Math.sqrt(Math.max(distance - minDistance, 0) / opacityFalloff));
+    //         }
+    //     });
+    // });
 }
 
 const ConnectionOpacityPlugin = {
