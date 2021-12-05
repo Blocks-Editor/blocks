1266c8867728bb7b8d044ba532d8a8ec9b404afe
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-12-05 16:34:45 -0700

    Tooltip boilerplate

--- packages/blocks-webapp/src/components/GlobalTooltip.js
+++ packages/blocks-webapp/src/components/GlobalTooltip.js
@@ -2,12 +2,16 @@ import styled from 'styled-components';
 import ReactTooltip from 'react-tooltip';
 import React from 'react';
 
-const ReactTooltipStyled = styled(ReactTooltip)`
+const StyledReactTooltip = styled(ReactTooltip)`
     padding: .5em 1em !important;
 `;
 
 export default function GlobalTooltip() {
+    const afterShow = (event) => {
+        // console.log(event);///
+    };
+
     return (
-        <ReactTooltipStyled backgroundColor="#111" place="bottom" delayShow={250}/>
+        <StyledReactTooltip backgroundColor="#111" place="bottom" delayShow={250} afterShow={afterShow}/>
     );
 }
