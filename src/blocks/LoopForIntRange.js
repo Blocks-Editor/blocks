d93aa7ac17d90277330a1434b1cb081d1a0aa194
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-12-08 01:55:07 -0700

    Add for-range loop block

---
+++ src/blocks/LoopForIntRange.js
@@ -0,0 +1,35 @@
+import {statementBlock} from '../block-patterns/statement-patterns';
+import {effectType, intType} from '../block-types/types';
+import {importRef, nodeIdentifierRef} from '../compilers/MotokoCompiler';
+
+export const iterImportRef = importRef('mo:base/Iter');
+
+const block = statementBlock({
+    title: 'for(min..max) {}',
+    info: 'Repeat for a range of integers (inclusive)',
+    inputs: [{
+        key: 'min',
+        type: intType,
+    }, {
+        key: 'max',
+        type: intType,
+    }, {
+        key: 'loop',
+        type: effectType,
+    }],
+    outputs: [{
+        key: 'item',
+        type: intType,
+        toMotoko(_, node) {
+            return nodeIdentifierRef(node);
+        },
+    }],
+    // controls:[{
+    //    key:'name',
+    //    type:identifierType,
+    //    optional:true,
+    // }],
+}, ({min, max, loop}, node) => {
+    return `for (${nodeIdentifierRef(node)} in ${iterImportRef}.range(${min}, ${max})) { ${loop ?? ''} };`;
+});
+export default block;
---
+++ src/blocks/ReferenceCall.js
@@ -0,0 +1,60 @@
+import {statementBlock} from '../block-patterns/statement-patterns';
+import {nodeType, valueType} from '../block-types/types';
+import {FaPlayCircle} from 'react-icons/fa';
+import {functionCategory} from '../block-categories/categories';
+import {getFunctionReturnType} from './Function';
+
+const block = statementBlock({
+    info: 'Invoke a function in this project',
+    title: 'Call Function',
+    category: functionCategory,
+    icon: FaPlayCircle,
+    computeTitle(node, editor) {
+        let functionNode = editor.compilers.node.getInput(node, 'functionNode');
+        if(!functionNode) {
+            return;
+        }
+        let name = editor.compilers.motoko.getInput(functionNode, 'name');
+        let paramNames = editor.compilers.node.getInput(functionNode, 'params')
+            .map(n => editor.compilers.motoko.getInput(n, 'name'));
+        return name && `${name}(${paramNames.join(', ')})`;
+    },
+    inputs: [{
+        key: 'args',
+        type: valueType,
+        multi: true, // TODO: separate arg sockets
+    }],
+    outputs: [{
+        key: 'value',
+        type: valueType,
+        inferType(_, node, compiler) {
+            let functionNode = compiler.editor.compilers.node.getInput(node, 'functionNode');
+            if(!functionNode) {
+                return;
+            }
+            return getFunctionReturnType(functionNode, compiler.editor);
+        },
+        toMotoko({args}, node, compiler) {
+            let functionNode = compiler.editor.compilers.node.getInput(node, 'functionNode');
+            if(!functionNode) {
+                return;
+            }
+            let name = compiler.getInput(functionNode, 'name');
+            if(!name) {
+                return;
+            }
+            return `${name}(${args.join(', ')})`;
+        },
+    }],
+    controls: [{
+        key: 'functionNode',
+        type: nodeType.withMeta({block: 'Function'}),
+    }],
+}, ({functionNode, args}, node, compiler) => {
+    let name = compiler.getInput(functionNode, 'name');
+    if(!name) {
+        return;
+    }
+    return `ignore ${name}(${args.join(', ')});`;
+});
+export default block;
