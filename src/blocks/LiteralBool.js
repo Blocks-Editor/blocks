12d63f082c0af56998ec9c76a2f9e0dd7eca0e60
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-12-12 16:24:22 -0700

    Add custom boolean value search logic

--- src/blocks/LiteralBool.js
+++ src/blocks/LiteralBool.js
@@ -3,5 +3,21 @@ import {boolType} from '../block-types/types';
 
 const block = literalBlock({
     title: 'Boolean',
+    customSearch(text) {
+        if(!text) {
+            return;
+        }
+        for(let value of [true, false]) {
+            let title = String(value);
+            if(title.startsWith(text.toLowerCase())) {
+                return {
+                    title,
+                    data: {
+                        value,
+                    },
+                };
+            }
+        }
+    },
 }, boolType);
 export default block;
