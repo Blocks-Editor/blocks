f69eda4218652db4d92bf57ddab83673739ebe69
Nate St. George <nateestes223@gmail.com>
2021-12-15 15:59:22 -0700

    Set dark mode to default theme

--- src/hooks/persistent/useThemeState.js
+++ src/hooks/persistent/useThemeState.js
@@ -1,12 +1,14 @@
 import useLocalStorage from '../utils/useLocalStorage';
 import useThemes from '../useThemes';
 
+const defaultTheme = 2;
+
 export default function useThemeState() {
     const themes = useThemes();
-    const [id, setId] = useLocalStorage('blocks.theme', themes[0].id);
+    const [id, setId] = useLocalStorage('blocks.theme', themes[defaultTheme].id);
 
     return [
-        themes.find(theme => theme.id === id) || themes[0],
+        themes.find(theme => theme.id === id) || themes[defaultTheme],
         id => setId(id),
     ];
 }
