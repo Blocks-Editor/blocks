c9ca4114c6fd7430bd1d3ab767a84337820d680e
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-12-16 18:29:01 -0700

    Tweak mobile page message formatting

--- src/components/MobilePage.js
+++ src/components/MobilePage.js
@@ -12,9 +12,7 @@ export default function MobilePage() {
             <span className="display-3 pb-5">Hey there!</span>
             <span className="text-muted px-5">
                 If you are interested in using Blocks on a mobile device,
-                please check out
-                <br/>
-                <a href="https://github.com/Blocks-Editor/blocks/issues/78">this GitHub discussion</a>.
+                please check out <a className="text-nowrap" href="https://github.com/Blocks-Editor/blocks/issues/78">this GitHub discussion</a>.
             </span>
         </MobilePanel>
     );
