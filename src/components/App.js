6379d97824ced214abfd29c38833c0453f8c2532
Nate St. George <nateestes223@gmail.com>
2021-12-21 13:23:42 -0700

    Quick Safari toast

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import EventsContext, {ERROR_EVENT} from '../contexts/EventsContext';
@@ -9,9 +9,21 @@ import TabLayout from './TabLayout';
 import GlobalTheme from './global/GlobalTheme';
 import GlobalObservables from './global/GlobalObservables';
 import MobilePage from './MobilePage';
+import { isSafari } from 'react-device-detect';
 
 export default function App() {
     const events = useContext(EventsContext);
+
+    useEffect(() => {
+        // Check user agent on load
+        if(isSafari) {
+            toast('Blocks currently has intermittent performance issues on Safari. For now, we recommend Chrome for the best experience.', {
+                autoClose: false
+            });
+        }
+    });
+
+
 
     useListener(events, ERROR_EVENT, err => {
         toast(err, {
@@ -26,7 +38,7 @@ export default function App() {
             <GlobalTooltip/>
             <BrowserRouter>
                 <GlobalTheme>
-                    <ToastContainer style={{marginTop: '5rem'}}/>
+                    <ToastContainer style={{marginTop: '5rem'}} position='top-center'/>
                     <div className="d-block d-sm-none w-100 h-100">
                         <MobilePage/>
                     </div>
