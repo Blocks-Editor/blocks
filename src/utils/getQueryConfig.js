a84d9be1d4182fb8abd43693a75d975778d3f27c
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-12-22 16:44:02 -0700

    Fix getQueryConfig() in testing environment

--- src/utils/getQueryConfig.js
+++ src/utils/getQueryConfig.js
@@ -3,5 +3,5 @@ import {parse} from 'querystring';
 const config = parse(window.location.search.substring(1));
 
 export default function getQueryConfig(key, defaultValue) {
-    return config.hasOwnProperty(key) ? config[key] : defaultValue;
+    return key in config ? config[key] : defaultValue;
 }
