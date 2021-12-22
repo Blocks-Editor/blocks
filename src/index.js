529897c13892add21af90f3e0c6786988c784d0c
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-12-21 19:04:19 -0700

    Add Firebase configuration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.scss';
 import App from './components/App';
+import './setupFirebase';
 
 ReactDOM.render(
     <React.StrictMode>
---
+++ src/setupFirebase.js
@@ -0,0 +1,17 @@
+import {initializeApp} from 'firebase/app';
+import {getAnalytics} from 'firebase/analytics';
+import {getFirestore} from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyB2Knl_5Z113bHF4ncVwf37qENayLs-TUY',
+    authDomain: 'motoko-blocks.firebaseapp.com',
+    projectId: 'motoko-blocks',
+    storageBucket: 'motoko-blocks.appspot.com',
+    messagingSenderId: '225162830482',
+    appId: '1:225162830482:web:5f4a0c292cd509bc6c3c4b',
+    measurementId: 'G-2V5DTR90FF',
+};
+
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
