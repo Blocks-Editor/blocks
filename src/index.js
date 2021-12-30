7022af35bfb1921b15b0ac3aa8e06614044e6a78
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-12-30 03:41:20 -0700

    Fix Sass index file import path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.scss';
+import './styles/index.scss';
 import App from './components/App';
 import './setupFirebase';
 
