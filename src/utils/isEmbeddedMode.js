8720fce94682486bf14d84611fb0ccc80cff8c85
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-01-13 21:20:55 -0700

    Add query param to enable embedded mode

--- src/utils/isEmbeddedMode.js
+++ src/utils/isEmbeddedMode.js
@@ -1,8 +1,10 @@
 // Derived from: https://stackoverflow.com/questions/326069/how-to-identify-if-a-webpage-is-being-loaded-inside-an-iframe-or-directly-into-t
 
+import getQueryConfig from './getQueryConfig';
+
 let embedded = true;
 try {
-    embedded = window.self !== window.top;
+    embedded = window.self !== window.top || getQueryConfig('embed');
 }
 catch(e) {
 }
