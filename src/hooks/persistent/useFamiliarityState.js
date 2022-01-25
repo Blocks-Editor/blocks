6d448b43f72240c3347c7de1dca275358bf4907b
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-01-25 15:11:54 -0700

    Add familiarity query parameter config

--- src/hooks/persistent/useFamiliarityState.js
+++ src/hooks/persistent/useFamiliarityState.js
@@ -1,8 +1,23 @@
 import useLocalStorage from '../utils/useLocalStorage';
+import getQueryConfig from '../../utils/getQueryConfig';
+import isEmbeddedMode from '../../utils/isEmbeddedMode';
 
 export const LEARNING = 'learning';
 export const FAMILIAR = 'familiar';
 
 export default function useFamiliarityState() {
-    return useLocalStorage('blocks.familiarity', null);
+    let familiarityConfig = getQueryConfig('familiarity', isEmbeddedMode() ? true : null);
+    if(familiarityConfig === 'true') {
+        // Already started tutorial
+        familiarityConfig = true;
+    }
+    else if(familiarityConfig === 'none') {
+        // Suggest tutorial
+        familiarityConfig = null;
+    }
+    else {
+        // Skipped tutorial
+        familiarityConfig = false;
+    }
+    return useLocalStorage('blocks.familiarity', familiarityConfig);
 }
