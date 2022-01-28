966a9f8913be0bb5e28c3184b330d51be8696d9d
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-01-28 11:29:39 -0700

    Clarify mobile shortcut button tutorial instructions

--- src/tutorials/utils/tutorialText.js
+++ src/tutorials/utils/tutorialText.js
@@ -3,4 +3,4 @@ import {isMobile} from 'react-device-detect';
 
 export const TUTORIAL_LEFT_CLICK = !isMobile ? 'click' : 'tap';
 export const TUTORIAL_CONTEXT_MENU_CLICK = !isMobile ? 'right click' : 'tap';
-export const TUTORIAL_CLICK_DRAG = !isMobile ? 'click and drag' : 'touch and drag';
+export const TUTORIAL_CLICK_DRAG = !isMobile ? 'click and drag' : 'tap and then drag';
