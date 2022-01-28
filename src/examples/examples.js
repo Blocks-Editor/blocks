2d91834628eef3b21bad9fbd376d6ea9b6386536
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-01-27 19:21:25 -0700

    Remove 'Counter' example from menu

--- src/examples/examples.js
+++ src/examples/examples.js
@@ -1,6 +1,6 @@
 const examples = [
     require('./files/Echo.blocks.json'),
-    require('./files/Counter.blocks.json'),
+    // require('./files/Counter.blocks.json'),
     require('./files/Calculator.blocks.json'),
     // require('./files/WhoAmI.blocks.json'),
     require('./files/PhoneBook.blocks.json'),
