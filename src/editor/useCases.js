52c11e595343edc242f1b41f19c16692a8e5ebc8
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-02-04 02:15:38 -0700

    Add global use case array

--- src/editor/useCases.js
+++ src/editor/useCases.js
@@ -1,12 +1,19 @@
-export const FOR_DEBUGGING = 'debugging';
-export const FOR_LEARNING_MOTOKO = 'learning Motoko';
-export const FOR_ASSIGNING_ID = 'assigning unique IDs';
-export const FOR_BUILDING_API = 'building an API';
-export const FOR_STORING_DATA = 'storing data';
-export const FOR_TRANSACTIONS = 'transaction logic';
-export const FOR_REUSABLE_LOGIC = 'writing reusable logic';
-export const FOR_ORGANIZATION = 'project organization';
-export const FOR_DOCUMENTATION = 'documentation';
-export const FOR_CONFIGURATION = 'configuration';
-export const FOR_CUSTOM_LOGIC = 'custom logic';
-export const FOR_ERROR_HANDLING = 'error handling';
+export const USE_CASES = [];
+
+export const FOR_LEARNING_MOTOKO = addUseCase('learning Motoko');
+export const FOR_BUILDING_API = addUseCase('building an API');
+export const FOR_STORING_DATA = addUseCase('storing data');
+export const FOR_TRANSACTIONS = addUseCase('transaction logic');
+export const FOR_REUSABLE_LOGIC = addUseCase('writing reusable logic');
+export const FOR_ORGANIZATION = addUseCase('project organization');
+export const FOR_ASSIGNING_ID = addUseCase('assigning unique IDs');
+export const FOR_DOCUMENTATION = addUseCase('documentation');
+export const FOR_CONFIGURATION = addUseCase('configuration');
+export const FOR_DEBUGGING = addUseCase('debugging');
+export const FOR_ERROR_HANDLING = addUseCase('error handling');
+export const FOR_CUSTOM_LOGIC = addUseCase('custom logic');
+
+function addUseCase(useCase) {
+    USE_CASES.push(useCase);
+    return useCase;
+}
